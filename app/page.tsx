d69e1e56250b6b099f7ba1ff078b1793960d23a6
Richard Alcaide <richardalcaide16@gmail.com>
2025-04-25 15:02:13 +0800

    fixed button label

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function SlackSchedulerPage() {
   const buttonLabel =
     delayAmount === ''
       ? 'Send'
-      : `Send in ${delayAmount} ${delayUnit}${delayAmount === 1 ? '' : 's'}`;
+      : `Send in ${delayAmount} ${delayUnit}`;
 
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-4 bg-gray-50">
